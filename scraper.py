ccc052135a1665b2c333105c17ffc0ed17d41a06
Whiteyeonline <www.muneerjahan@gmail.com>
2025-06-04 16:32:35 +0530

    Delete scraper.py

--- scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def scrape_data(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    title = soup.title.string if soup.title else "No Title Found"
-    return f"Title: {title}"
